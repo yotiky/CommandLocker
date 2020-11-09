630f13630eb927f7da80f82724ec45e0a46c483f
yotiky <yotiky@gmail.com>
2020-11-10 07:06:51 +0900

    add: CommandLocker.

---
+++ CommandLocker/CommandLocker/CommandLocker.cs
@@ -0,0 +1,102 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLocker
+{
+    public class CommandLocker<T> where T : IEquatable<T>
+    {
+        public ReactiveProperty<bool> CanExecute { get; }
+        private ReactiveProperty<bool> canExcuteWithTrigger { get; }
+        public ReactiveProperty<bool> CanExecuteWithTrigger { get; }
+
+        private Guid lockKey;
+        private object lockObject = new object();
+
+        private Guid lockTriggerKey;
+        private T releaseTrigger;
+
+        public CommandLocker()
+        {
+            CanExecute = new ReactiveProperty<bool>(true);
+            canExcuteWithTrigger = new ReactiveProperty<bool>(true);
+            CanExecuteWithTrigger = CanExecute
+                .CombineLatest(canExcuteWithTrigger, (a, b) => a && b)
+                .ToReactiveProperty();
+        }
+
+        public (bool result, Guid key) Lock()
+        {
+            if (lockKey != Guid.Empty) { return (false, Guid.Empty); }
+
+            lock (lockObject)
+            {
+                CanExecute.Value = false;
+                lockKey = Guid.NewGuid();
+            }
+            return (true, lockKey);
+        }
+        public bool Release(Guid key)
+        {
+            if (lockKey != key) { return false; }
+
+            lock (lockObject)
+            {
+                CanExecute.Value = true;
+                lockKey = Guid.Empty;
+            }
+            return true;
+        }
+
+        public (bool, Guid) Lock(T trigger)
+        {
+            if (lockTriggerKey != Guid.Empty) { return (false, Guid.Empty); }
+
+            lock (lockObject)
+            {
+                canExcuteWithTrigger.Value = false;
+                lockTriggerKey = Guid.NewGuid();
+                releaseTrigger = trigger;
+            }
+            return (true, lockTriggerKey);
+        }
+        public bool TryRelease(T value, Guid? key = null)
+        {
+            if (lockTriggerKey == Guid.Empty) { return false; }
+            if (!releaseTrigger.Equals(value)) { return false; }
+            if (key != null && lockTriggerKey != key) { return false; }
+
+            lock (lockObject)
+            {
+                canExcuteWithTrigger.Value = true;
+                lockTriggerKey = Guid.Empty;
+            }
+            return true;
+        }
+
+        public async Task WaitReleaseAsync()
+        {
+            if (canExcuteWithTrigger.Value) { return; }
+
+            var result = false;
+            while (!result)
+            {
+                result = await canExcuteWithTrigger;
+            }
+
+        }
+        public void ReleaseForce()
+        {
+            lock (lockObject)
+            {
+                CanExecute.Value = true;
+                canExcuteWithTrigger.Value = true;
+                lockKey = Guid.Empty;
+                lockTriggerKey = Guid.Empty;
+            }
+        }
+    }
+}
---
+++ CommandLocker/CommandLocker/MainWindowViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace CommandLocker
+{
+    public class MainWindowViewModel
+    {
+        public ReactiveCommand DefaultLockDemoCommand { get; }
+        public ReactiveCommand TriggerLockDemoCommand { get; }
+        public ReactiveCommand LockCommand { get; }
+        public ReactiveCommand UnlockCommand { get; }
+        public ReactiveCommand LockTriggerCommand { get; }
+        public ReactiveCommand UnlockTriggerCommand { get; }
+        public ReactiveCommand UnlockForceCommand { get; }
+        public AsyncReactiveCommand WaitUnlockTriggerDemoCommand { get; }
+
+        public ReactiveProperty<string> Log { get; } = new ReactiveProperty<string>();
+
+        private CommandLocker<int> locker = new CommandLocker<int>();
+
+        public MainWindowViewModel()
+        {
+            DefaultLockDemoCommand = new ReactiveCommand(locker.CanExecute);
+            TriggerLockDemoCommand = new ReactiveCommand(locker.CanExecuteWithTrigger);
+            LockCommand = new ReactiveCommand();
+            UnlockCommand = new ReactiveCommand();
+            LockTriggerCommand = new ReactiveCommand();
+            UnlockTriggerCommand = new ReactiveCommand();
+            UnlockForceCommand = new ReactiveCommand();
+            WaitUnlockTriggerDemoCommand = new AsyncReactiveCommand();
+
+            var key = Guid.Empty;
+            LockCommand.Subscribe(() =>
+            {
+                var locked = locker.Lock();
+                if (locked.result) { key = locked.key; }
+            });
+            UnlockCommand.Subscribe(() =>
+            {
+                locker.Release(key);
+                key = Guid.Empty;
+            });
+
+            var count = 0;
+            LockTriggerCommand.Subscribe(() =>
+            {
+                var locked = locker.Lock(3);
+            });
+            UnlockTriggerCommand.Subscribe(() =>
+            {
+                if (key == Guid.Empty) { count++; }
+
+                var result = locker.TryRelease(count);
+                if (result) { count = 0; }
+            });
+            UnlockForceCommand.Subscribe(() =>
+            {
+                locker.ReleaseForce();
+                count = 0;
+            });
+            WaitUnlockTriggerDemoCommand.Subscribe(async () =>
+            {
+                var locked = locker.Lock(3);
+                Log.Value = "Locked.";
+                await locker.WaitReleaseAsync();
+                Log.Value = "Unlocked.";
+            });
+        }
+    }
+}
